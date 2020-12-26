67f31753cbfd771aab9cf9a9e833abfd69617c42
Tushar Khandejod <43365372+tusharkhandejod@users.noreply.github.com>
2020-12-26 09:12:43 +0530

    UC1-Code for validating User's name is added

--- JavaScript/inputForm1.js
+++ JavaScript/inputForm1.js
@@ -5,3 +5,27 @@ output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
+//UC1-Validating user first name
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text.value)) {
+        textError.textContent = "";
+    } else {
+        textError.textContent = "First Name is incorrect";
+    }
+});
+
+//Validating user last name
+const text2 = document.querySelector('#text2');
+const text2Error = document.querySelector('.text2-error');
+text2.addEventListener('input', function() {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text2.value)) {
+        text2Error.textContent = "";
+    } else {
+        text2Error.textContent = "Last Name is incorrect";
+    }
+});
